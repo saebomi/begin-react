b31509e213f68ef5466e44c4d07d36ba36022e0c
saebomi <saebomi0324@naver.com>
2019-09-14 01:28:39 +0900

    jsx 문법기초알기

--- src/App.js
+++ src/App.js
@@ -1,26 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Hello from './Hello'
+import './App.css'
 
 function App() {
+  const name='react';
+  const style = {
+    backgroundColor: 'black',
+    color:'aqua',
+    fontSize:24,
+    padding:'1rem'
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <>
+      <Hello />
+      <div style={style}>{name}</div>
+      <div className="gray-box"></div>
+    </>
+   );
 }
 
 export default App;
+
