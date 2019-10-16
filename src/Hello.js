0fdfb8634ea946ecfe0bf0a991cebc5acc5c6ea3
saebomi <saebomi0324@naver.com>
2019-10-16 13:22:42 +0900

    여러개의 props,비구조화 할당

--- src/Hello.js
+++ src/Hello.js
@@ -1,10 +1,13 @@
 import React from 'react'; // 컴포넌트를 불러와서 사용하겠다
 
 //컴포넌트 만드는 방법 1) 함수형태 2)클래스형태
-function Hello({color,name}){
-    return <div style={{
+function Hello(props){
+    /*return <div style={{
         color:color
-    }}>안녕하세요 {name}</div>;
+    }}>안녕하세요 {name}</div>;*/
+    return <div>안녕하세요 {props.name}</div>
+    // props > 객체 형태로 전달됨
+    // name 값 조회 > props.name 을 조회
 }
 
 Hello.defaultProps = {
