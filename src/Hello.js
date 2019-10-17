99cdef6c39e19d0b14b295eadb7ec7194a193677
saebomi <saebomi0324@naver.com>
2019-10-17 13:13:58 +0900

    props로 컴포넌트에 값전달

--- src/Hello.js
+++ src/Hello.js
@@ -1,16 +1,13 @@
 import React from 'react'; // 컴포넌트를 불러와서 사용하겠다
 
 //컴포넌트 만드는 방법 1) 함수형태 2)클래스형태
-function Hello(props){
-    /*return <div style={{
-        color:color
-    }}>안녕하세요 {name}</div>;*/
-    return <div>안녕하세요 {props.name}</div>
+function Hello({ color, name }) {
+    return <div style={{ color }}>안녕하세요 {name}</div>
     // props > 객체 형태로 전달됨
     // name 값 조회 > props.name 을 조회
-}
-
-Hello.defaultProps = {
-    name:'이름없음'
-}
+  }
+  
+  Hello.defaultProps = {
+    name: '이름없음'
+  }
 export default Hello;
