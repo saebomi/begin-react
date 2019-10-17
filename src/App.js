b611d1114aa6a6d9b5aa32017b4480d69f0314be
saebomi <saebomi0324@naver.com>
2019-10-17 13:17:06 +0900

    props로 컴포넌트에 값전달

--- src/App.js
+++ src/App.js
@@ -1,27 +1,17 @@
 import React from 'react';
-import Counter from './Counter';
-//import Hello from './Hello'
+//import Counter from './Counter';
+import Hello from './Hello';
+import Wrapper from './Wrapper';
 //import './App.css'
 
 function App() {
   return(
-    <Counter />
+    <Wrapper>
+    <Hello name="react" color="red"/>
+    <Hello color="pink"/>
+    </Wrapper>
   );
 }
- /* const name='react';
-  const style = {
-    backgroundColor: 'black',
-    color:'aqua',
-    fontSize:24,
-    padding:'1rem'
-  }
-  return (
-    <>
-      <Hello />
-      <div style={style}>{name}</div>
-      <div className="gray-box"></div>
-    </>
-   );*/
-
+   
 export default App;
 
