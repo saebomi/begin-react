de8662576d0bf8ef3108ec3133fef23ea733f3f4
saebomi <saebomi0324@naver.com>
2019-10-18 13:04:20 +0900

    조건부렌더링

--- src/Hello.js
+++ src/Hello.js
@@ -1,8 +1,12 @@
 import React from 'react'; // 컴포넌트를 불러와서 사용하겠다
 
 //컴포넌트 만드는 방법 1) 함수형태 2)클래스형태
-function Hello({ color, name }) {
-    return <div style={{ color }}>안녕하세요 {name}</div>
+function Hello({ color, name, isSpecial }) {
+    return <div style={{ color }}>
+      { isSpecial && <b>*</b>} 
+      {/*단축 평가 논리 계산법 참고*/}
+      안녕하세요 {name}
+    </div>
     // props > 객체 형태로 전달됨
     // name 값 조회 > props.name 을 조회
   }
