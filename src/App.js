668bd04e7876936dc1138152b4dea4a09fe712b7
saebomi <saebomi0324@naver.com>
2019-10-19 23:51:25 +0900

    이벤트설정

--- src/App.js
+++ src/App.js
@@ -1,16 +1,17 @@
 import React from 'react';
-//import Counter from './Counter';
-import Hello from './Hello';
-import Wrapper from './Wrapper';
+import Counter from './Counter';
+//import Hello from './Hello';
+//import Wrapper from './Wrapper';
 //import './App.css'
 
 function App() {
   return(
-    <Wrapper>
+    /*<Wrapper>
     <Hello name="react" color="red" isSpecial={true}/>
-    {/* isSpecial 이름만 넣어주면 isSpecial={true} 와 동일한 의미 */}
+    // isSpecial 이름만 넣어주면 isSpecial={true} 와 동일한 의미
     <Hello color="pink"/>
-    </Wrapper>
+    </Wrapper>*/
+    <Counter />
   );
 }
    
