56a0a34323ec1c9229661614c8b08ea6a35bfb18
saebomi <saebomi0324@naver.com>
2019-10-19 23:49:51 +0900

    이벤트설정

--- src/Counter.js
+++ src/Counter.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function Counter(){
+    /* 이벤트 설정 */
+    const onIncrease = () => {
+        console.log('+1')
+    }
+    const onDecrease = () => {
+        console.log('-1');
+    }
     return(
         <div>
             <h1>0</h1>
-            <button>+1</button>
-            <button>-1</button>
+            <button onClick={onIncrease}>+1</button>
+            <button onClick={onDecrease}>-1</button>
         </div>
     );
 }
