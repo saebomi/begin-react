8a9609dcc8cae78c7572da3bc2ba866317c23236
saebomi <saebomi0324@naver.com>
2019-10-20 20:25:13 +0900

    useState 예제

--- src/Counter.js
+++ src/Counter.js
@@ -1,16 +1,20 @@
 import React, { useState } from 'react';
+/* 리액트 패키지에서 useState 라는 함수를 불러옴 */
 
 function Counter(){
-    /* 이벤트 설정 */
+    const [number, setNumber] = useState(0);
+    /* usestate 를 사용할 때는 상태의 기본값을 파라미터로 넣어서 호출 */
+
+    /* 이벤트 설정  */
     const onIncrease = () => {
-        console.log('+1')
+        setNumber(number+1);
     }
     const onDecrease = () => {
-        console.log('-1');
+        setNumber(number-1);
     }
     return(
         <div>
-            <h1>0</h1>
+            <h1>{number}</h1>
             <button onClick={onIncrease}>+1</button>
             <button onClick={onDecrease}>-1</button>
         </div>
