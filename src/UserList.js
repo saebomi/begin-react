84f5344148b357d0b470ef7fb73f079eda987fe0
saebomi <saebomi0324@naver.com>
2019-10-24 00:28:30 +0900

    배열에 항목제거하기

--- src/UserList.js
+++ src/UserList.js
@@ -1,17 +1,18 @@
 import React from 'react';
 
-function User({ user }){
+function User({ user, onRemove }){
     return(
         <div>
             <b>{user.username}</b><span>({user.email})</span>
+            <button onClick={() => onRemove(user.id)}>삭제</button>
         </div>
     );
 }
-function UserList({users}) {
+function UserList({users, onRemove}) {
     return(
         <div>
                 {users.map(user =>(
-                    <User user={user} key={user.id}/>
+                    <User user={user} key={user.id} onRemove={onRemove}/>
                 ))}
                 {/*<User user={users[0]} />
                 <User user={users[1]} />
