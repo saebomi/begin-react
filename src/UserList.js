1fa5617e3e75de34a2899de7f52e07e4467fec01
saebomi <saebomi0324@naver.com>
2019-10-23 13:16:41 +0900

    userRef 예제

--- src/UserList.js
+++ src/UserList.js
@@ -5,36 +5,17 @@ function User({ user }){
         <div>
             <b>{user.username}</b><span>({user.email})</span>
         </div>
-    )
+    );
 }
-function UserList() {
-    const users = [
-        {
-            id: 1,
-            username: 'bom',
-            email: 'saebomi000324@gmail.com'
-        },
-        {
-            id: 2,
-            username: 'tester',
-            email: 'tester@example.com'
-        },
-        {
-            id: 3,
-            username: 'liz',
-            email: 'liz@example.com'
-        }
-    ];
+function UserList({users}) {
     return(
         <div>
-            <div>
                 {users.map(user =>(
                     <User user={user} key={user.id}/>
                 ))}
                 {/*<User user={users[0]} />
                 <User user={users[1]} />
                 <User user={users[2]} />*/}
-            </div>
         </div>
     );
 }
