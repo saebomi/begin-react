a02e7d29e63f2e029262c5e2d42a9639e014ce9d
saebomi <saebomi0324@naver.com>
2019-10-24 13:15:14 +0900

    배열 항목 수정하기

--- src/UserList.js
+++ src/UserList.js
@@ -1,24 +1,32 @@
 import React from 'react';
 
-function User({ user, onRemove }){
-    return(
-        <div>
-            <b>{user.username}</b><span>({user.email})</span>
-            <button onClick={() => onRemove(user.id)}>삭제</button>
-        </div>
-    );
+function User({ user, onRemove, onToggle }) {
+  return (
+    <div>
+      <b
+        style={{
+          cursor: 'pointer',
+          color: user.active ? 'green' : 'black'
+        }}
+        onClick={() => onToggle(user.id)}
+      >
+        {user.username}
+      </b>
+        &nbsp;
+      <span>({user.email})</span>
+      <button onClick={() => onRemove(user.id)}>삭제</button>
+    </div>
+  );
 }
-function UserList({users, onRemove}) {
-    return(
-        <div>
-                {users.map(user =>(
-                    <User user={user} key={user.id} onRemove={onRemove}/>
-                ))}
-                {/*<User user={users[0]} />
-                <User user={users[1]} />
-                <User user={users[2]} />*/}
-        </div>
-    );
+
+function UserList({ users, onRemove, onToggle }) {
+  return (
+    <div>
+      {users.map(user => (
+        <User user={user} key={user.id} onRemove={onRemove} onToggle={onToggle}/>
+      ))}
+    </div>
+  );
 }
 
 export default UserList;
