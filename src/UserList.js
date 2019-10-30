ab429a896a49db0fe442a62c49a1fb88bbc93e82
saebomi <saebomi0324@naver.com>
2019-10-30 13:14:03 +0900

    deps에 특정값 넣기

--- src/UserList.js
+++ src/UserList.js
@@ -1,12 +1,14 @@
-import React, { userEffect } from 'react';
+import React, { useEffect } from 'react';
 
 function User({ user, onRemove, onToggle }) {
   useEffect(() => {
-    console.log('컴포넌트가 화면에 나타남');
-    return () =>{
-      console.log('컴포넌트가 화면에서 사라짐');
+    //console.log('user 값이 설정됨');
+    console.log(user);
+    /*return () => {
+      console.log('user가 바뀌기 전..');
+      console.log(user);
     };
-  },[]);
+  }, [user]*/});
   return (
     <div>
       <b
@@ -18,7 +20,7 @@ function User({ user, onRemove, onToggle }) {
       >
         {user.username}
       </b>
-        &nbsp;
+      &nbsp;
       <span>({user.email})</span>
       <button onClick={() => onRemove(user.id)}>삭제</button>
     </div>
@@ -29,11 +31,12 @@ function UserList({ users, onRemove, onToggle }) {
   return (
     <div>
       {users.map(user => (
-        <User 
-        user={user} 
-        key={user.id} 
-        onRemove={onRemove} 
-        onToggle={onToggle}/>
+        <User
+          user={user}
+          key={user.id}
+          onRemove={onRemove}
+          onToggle={onToggle}
+        />
       ))}
     </div>
   );
