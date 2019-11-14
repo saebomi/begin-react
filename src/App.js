8968f39142e8f8ffde177edc13b6a5ca08f87dea
saebomi <saebomi0324@naver.com>
2019-11-14 13:10:30 +0900

    커스텀 hook예제

--- src/App.js
+++ src/App.js
@@ -1,101 +1,107 @@
-import React, {useRef, useState, useMemo, useCallback} from 'react';
+import React, { useRef, useReducer, useMemo, useCallback } from 'react';
 import UserList from './UserList';
 import CreateUser from './CreateUser';
+import useInputs from './hooks/useInputs';
 
-function countActiveUsers(users){
+function countActiveUsers(users) {
   console.log('활성 사용자 수를 세는중...');
   return users.filter(user => user.active).length;
 }
-// countActiveUsers 함수 > active 값이 true인 사용자의 수를 세는 역할
+
+const initialState = {
+  users: [
+    {
+      id: 1,
+      username: 'velopert',
+      email: 'public.velopert@gmail.com',
+      active: true
+    },
+    {
+      id: 2,
+      username: 'tester',
+      email: 'tester@example.com',
+      active: false
+    },
+    {
+      id: 3,
+      username: 'liz',
+      email: 'liz@example.com',
+      active: false
+    }
+  ]
+};
+
+function reducer(state, action) {
+  switch (action.type) {
+    case 'CREATE_USER':
+      return {
+        users: state.users.concat(action.user)
+      };
+    case 'TOGGLE_USER':
+      return {
+        users: state.users.map(user =>
+          user.id === action.id ? { ...user, active: !user.active } : user
+        )
+      };
+    case 'REMOVE_USER':
+      return {
+        users: state.users.filter(user => user.id !== action.id)
+      };
+    default:
+      return state;
+  }
+}
 
 function App() {
-  const [inputs, setInputs] = useState({
+  const [{ username, email }, onChange, reset] = useInputs({
     username: '',
-    email:''
+    email: ''
   });
-  const {username, email} = inputs;
-  const onChange = e => {
-    const {name, value} = e.target;
-    setInputs({
-      ...inputs,
-      [name]:value
-    });
-  };
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const nextId = useRef(4);
 
-  const [users, setUsers] = useState([
-    {
-      id:1,
-      username:'saebom',
-      email:'saebomi000324@gmail.com'
-    },
-    {
-      id:2,
-      username:'tester',
-      email:'tester@example.com'
-    },
-    {
-      id:3,
-      username:'liz',
-      email:'liz@example.com'
-    }
-  ]);
+  const { users } = state;
 
-  const nextId = useRef(4); 
-  // useRef()를 사용할 때 파라미터를 넣어주면, 이 값이 .current 값의 기본값!
   const onCreate = useCallback(() => {
-    const user={
-      id: nextId.current,
-      username,
-      email
-  };
-  setUsers(users => users.concat(user));
-
-  setInputs({
-      username: '',
-      email: ''
+    dispatch({
+      type: 'CREATE_USER',
+      user: {
+        id: nextId.current,
+        username,
+        email
+      }
     });
-    nextId.current +=1;
-  }, [username, email]);
+    reset();
+    nextId.current += 1;
+  }, [username, email, reset]);
 
-  const onRemove = useCallback(
-    id =>{
-    // user.id가 파라미터로 일치하지 않는 원소만 추출해서 새로운 배열을 만듬
-    // = user.id 가 id인 것을 제거함
-    setUsers(users.filter(user => user.id !== id));
-  }, [users]);
+  const onToggle = useCallback(id => {
+    dispatch({
+      type: 'TOGGLE_USER',
+      id
+    });
+  }, []);
 
-  const onToggle = useCallback(
-    id =>{
-    setUsers(
-      users.map(user =>
-        user.id === id ? {...user, active: !user.active } :user)
-    );
-  },[users]);
+  const onRemove = useCallback(id => {
+    dispatch({
+      type: 'REMOVE_USER',
+      id
+    });
+  }, []);
 
   const count = useMemo(() => countActiveUsers(users), [users]);
   return (
     <>
-    <CreateUser 
-      username={username}
-      email={email}
-      onChange={onChange}
-      onCreate={onCreate}
-    />
-      <UserList users={users} onRemove={onRemove} onToggle={onToggle} />
+      <CreateUser
+        username={username}
+        email={email}
+        onChange={onChange}
+        onCreate={onCreate}
+      />
+      <UserList users={users} onToggle={onToggle} onRemove={onRemove} />
       <div>활성사용자 수 : {count}</div>
     </>
   );
 }
-  /*return(
-    <UserList />
-    /*<Wrapper>
-    <Hello name="react" color="red" isSpecial={true}/>
-    // isSpecial 이름만 넣어주면 isSpecial={true} 와 동일한 의미
-    <Hello color="pink"/>
-    </Wrapper>
-    <Counter />
-    <InputSample />
-  );
-}*/
-export default App;
 
+export default App;
